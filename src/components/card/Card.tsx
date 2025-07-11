bb12242efad4ae620816c84ce326b705d15c1fc5
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-11 16:54:22 +0300

    add: plugin to remove clg

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import styles from "./Card.module.scss";
 export class Card extends Component {
   render() {
+    console.log("render");
     return (
       <>
         <div className={styles.card}>{}</div>
