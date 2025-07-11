0c2e0157e9884b09774bb99b396f12ca2a79e3a8
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-11 16:48:34 +0300

    fix:classes component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,14 +1,10 @@
-import { Component, type ReactNode } from "react";
+import { Component } from "react";
 import styles from "./Card.module.scss";
-
 export class Card extends Component {
-  render(): ReactNode {
+  render() {
     return (
       <>
-        <div className={styles.card}>
-          <h2 className={styles.title}>Card Title</h2>
-          <p className={styles.content}>Card content goes here.</p>
-        </div>
+        <div className={styles.card}>{}</div>
       </>
     );
   }
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,11 +1,15 @@
+import { Component, type ReactNode } from "react";
 import { Card } from "../card/Card";
 import styles from "./index.module.scss";
-export default function Main() {
-  return (
-    <>
-      <div className={styles.main}>
-        <Card />
-      </div>
-    </>
-  );
+
+export default class Main extends Component {
+  render(): ReactNode {
+    return (
+      <>
+        <div className={styles.main}>
+          <Card />
+        </div>
+      </>
+    );
+  }
 }
