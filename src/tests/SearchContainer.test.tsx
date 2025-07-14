e3bfa16ac88037e4659438255e6acc37fb52149e
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-14 22:29:51 +0300

    add: test

---
+++ src/tests/SearchContainer.test.tsx
@@ -0,0 +1,16 @@
+import SearchContainer from "@/components/search-container/SearchContainer";
+import type { ButtonProps } from "@/types/types";
+import { render } from "@testing-library/react";
+
+export const propsButton: ButtonProps = {
+  name: "name-button",
+  className: "btn-class-name",
+  type: "submit",
+};
+test("render SearchContainer", () => {
+  const form = document.createElement("form");
+
+  render(<SearchContainer />, {
+    baseElement: form,
+  });
+});
