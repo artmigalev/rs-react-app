d66b2c8b1a2dafaabf7c6b4f6b834c585df04264
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-14 19:01:20 +0300

    add: vitest to project

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import removeConsole from "vite-plugin-remove-console";
+import { configDefaults } from "vitest/config";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,4 +10,27 @@ export default defineConfig({
     tsconfigPaths(),
     removeConsole({ custom: ["console.log()", "console.warn()", "debugger"] }),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setup-tests.ts",
+    exclude: [...configDefaults.exclude],
+    passWithNoTests: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text"],
+      enabled: true,
+      cleanOnRerun: true,
+    },
+    deps: {
+      optimizer: {
+        web: { enabled: true },
+      },
+    },
+    watch: true,
+    root: "./src/",
+    dir: "./src/tests/",
+    clearMocks: true,
+    mockReset: true,
+  },
 });
