6bbead0080a7118bc7ecd4eb1a46c110ea5eeb20
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-16 14:44:20 +0300

    feat: loader to main

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,16 @@
+import { Component, type ReactNode } from "react";
+import styles from "./Loader.module.scss";
+export default class Loader extends Component {
+  render(): ReactNode {
+    return (
+      <>
+        <div className={styles.loader}>
+          <div className={`${styles["load-inner"]} ${styles["load-one"]}`}></div>
+          <div className={`${styles["load-inner"]} ${styles["load-two"]}`}></div>
+          <div className={`${styles["load-inner"]} ${styles["load-three"]}`}></div>
+          <span className={styles.text}>Loading...</span>
+        </div>
+      </>
+    );
+  }
+}
