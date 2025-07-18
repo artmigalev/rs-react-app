fe055286e2fba017914cdfca4463a0764cf85f0c
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-18 18:56:35 +0300

    feat:container with cards

---
+++ src/components/container/ContainerCards.tsx
@@ -0,0 +1,24 @@
+import { Component, type Context, type ReactNode } from "react";
+import type { ContainerCardsState } from "@/types/interface";
+import Loader from "../loader/Loader";
+import styles from "./ContainerCards.module.scss";
+import React from "react";
+import { SearchValueContext, type ContextSearchValue } from "@/context/SearchContext";
+
+export default class ContainerCards extends Component<ContainerCardsState> {
+  static contextType: Context<ContextSearchValue> = SearchValueContext;
+
+  declare context: React.ContextType<typeof SearchValueContext>;
+
+  state: Readonly<ContainerCardsState> = {
+    loading: true,
+    characters: [],
+  };
+
+  render(): ReactNode {
+    if (this.state) return <Loader />;
+    console.log(this.state);
+
+    return <div className={styles.container}></div>;
+  }
+}
