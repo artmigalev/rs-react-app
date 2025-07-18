270f03900a23410997c15311375a60cc7dc8b220
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-18 18:59:41 +0300

    add: type to ContainerCard state

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,6 +1,12 @@
 import type { ReactElement } from "react";
+import type { components } from "./stapi";
 
 export interface FormSearchProps {
   input: ReactElement;
   button: ReactElement;
+}
+
+export interface ContainerCardsState {
+  loading: boolean;
+  characters: components["schemas"]["CharacterBaseResponse"]["characters"] | [];
 }
