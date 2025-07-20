b41f5077a0e04116519854f74b79124f9f76f62a
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-20 19:07:00 +0300

    feat: cards to container and filter

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,18 +2,54 @@ import type { components } from "@/types/stapi";
 import { Component } from "react";
 import styles from "./Card.module.scss";
 
-export class Card extends Component<components["schemas"]["AnimalFull"]> {
+type CharacterProps = Pick<
+  components["schemas"]["CharacterFull"],
+  | "name"
+  | "hologramActivationDate"
+  | "hologram"
+  | "hologramStatus"
+  | "gender"
+  | "yearOfBirth"
+  | "monthOfBirth"
+  | "dayOfBirth"
+  | "placeOfDeath"
+  | "height"
+  | "weight"
+>;
+
+export class Card extends Component<CharacterProps> {
   render() {
-    console.log("render");
+    console.log(this.props);
+
+    const {
+      name,
+      hologramActivationDate,
+      hologram,
+      hologramStatus,
+      gender,
+      yearOfBirth,
+      monthOfBirth,
+      dayOfBirth,
+      placeOfDeath,
+      height,
+      weight,
+    } = this.props;
     return (
-      <>
-        <div className={styles.card}>
-          {
-            <h2 className="">{this.props.name}</h2>
-            // <span className="">{data}</span>
-          }
-        </div>
-      </>
+      <div className={styles["card"]}>
+        <h2 className={styles["name"]}>Character name {name}</h2>
+        <ul className={styles["list"]}>
+          <li>Gender {gender ?? "Unknown"}</li>
+          <li>Year the character was born {yearOfBirth ?? "Unknown"}</li>
+          <li> Month the character was born {monthOfBirth ?? "Unknown"}</li>
+          <li>Day the character was born {dayOfBirth ?? "Unknown"}</li>
+          <li>Place of birth {placeOfDeath ?? "Unknown"}</li>
+          <li>Height in centimeters {height ?? "Unknown"}</li>
+          <li>Weight in kilograms {weight ?? "Unknown"}</li>
+          <li>Hologram activation date {hologramActivationDate}</li>
+          <li>Hologram status {hologramStatus}</li>
+          <li>Whether this character is a hologram {hologram}</li>
+        </ul>
+      </div>
     );
   }
 }
--- src/components/container/ContainerCards.tsx
+++ src/components/container/ContainerCards.tsx
@@ -4,6 +4,8 @@ import Loader from "../loader/Loader";
 import styles from "./ContainerCards.module.scss";
 import React from "react";
 import { SearchValueContext } from "@/context/SearchContext";
+import getCharacters from "@/api/Character/getCharacters";
+import { Card } from "../card/Card";
 
 export default class ContainerCards extends Component<
   Record<string, unknown>,
@@ -15,19 +17,77 @@ export default class ContainerCards extends Component<
   state: Readonly<ContainerCardsState> = {
     loading: true,
     characters: [],
-    value: "value default",
+    value: "",
+    pageNumber: 1,
   };
+  loadMore = () => {
+    this.setState((prevState) => ({
+      pageNumber: prevState.pageNumber + 1,
+    }));
+  };
+  async componentDidMount() {
+    await this.getData();
+    this.setState({ loading: false });
+  }
 
-  componentDidUpdate(): void {
+  async componentDidUpdate(
+    prevProps: Readonly<Record<string, unknown>>,
+    prevState: Readonly<ContainerCardsState>
+  ) {
     const { searchValue } = this.context;
-    if (searchValue && searchValue !== this.state.value) {
-      console.log(searchValue, "ContainerCards searchValue");
-      this.setState({ value: searchValue });
+
+    if (searchValue !== this.state.value) {
+      this.setState(
+        {
+          value: searchValue ?? "",
+          pageNumber: 1,
+          characters: [],
+          loading: true,
+        },
+        async () => {
+          await this.getData();
+          this.setState({ loading: false });
+        }
+      );
+    }
+
+    if (this.state.pageNumber !== prevState.pageNumber && searchValue === this.state.value) {
+      await this.getData();
     }
   }
+
+  async getData() {
+    try {
+      const response = await getCharacters(this.state.pageNumber);
+      const { characters } = response;
+
+      this.setState((prevState) => ({
+        characters: [...prevState.characters, ...characters],
+      }));
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Fetch error:", error.message);
+      } else {
+        console.error("Unknown fetch error");
+      }
+    }
+  }
+
   render(): ReactNode {
     if (this.state.loading) return <Loader />;
+    const { characters } = this.state;
+    const { searchValue } = this.context;
 
-    return <div className={styles.container}></div>;
+    const lowerSearchVValue = (searchValue ?? "").trim().toLowerCase();
+    const filteredCharacters = lowerSearchVValue
+      ? characters.filter((character) => character.name?.toLowerCase().includes(lowerSearchVValue))
+      : characters;
+    return (
+      <div className={styles.container}>
+        {filteredCharacters.map((character) => (
+          <Card key={character.uid} {...character} />
+        ))}
+      </div>
+    );
   }
 }
