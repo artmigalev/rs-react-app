c6da1def0dc6745c87c3441a664130c871bed4b2
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-07-23 23:47:07 +0300

    feat: refactor your class components to functional components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,10 @@
-import Header from "@/components/layout/Header";
+import { Header } from "@/components/layout/Header";
 import Main from "@/components/layout/Main";
 import Footer from "@/components/layout/Footer";
 import "./App.css";
 import React, { useRef, useState } from "react";
 import { SearchValueContext } from "@/context/SearchContext";
 import type ContainerCards from "@/components/container/ContainerCards";
-
-// class App extends Component {
-//   containerRef = React.createRef<ContainerCards>();
-//   state = {
-//     searchValue: localStorage.getItem("searchText") || "",
-//   };
-//   setSearchValue = (value: string) => {
-//     this.setState({ searchValue: value });
-//   };
-//   loadMore = () => {
-//     this.containerRef.current?.loadMore();
-//   };
-//   render(): React.ReactNode {
-//     return (
-//       <>
-//         <SearchValueContext.Provider
-//           value={{
-//             searchValue: this.state.searchValue,
-//             setSearchValue: this.setSearchValue,
-//             loadMore: this.loadMore,
-//           }}
-//         >
-//           <Header />
-//           <Main containerRef={this.containerRef} />
-//           <Footer />
-//         </SearchValueContext.Provider>
-//       </>
-//     );
-//   }
-// }
 
 const App = (): React.JSX.Element => {
   const containerRef = useRef<ContainerCards>(null);
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,10 @@
-import SearchContainer from "@components/search-container/SearchContainer";
+import { SearchContainer } from "@components/search-container/SearchContainer";
 import styles from "./index.module.scss";
-import { Component, type ReactNode } from "react";
 
-export default class Header extends Component {
-  render(): ReactNode {
-    return (
-      <header className={styles.header}>
-        <SearchContainer />
-      </header>
-    );
-  }
-}
+export const Header = (): React.JSX.Element => {
+  return (
+    <header className={styles.header}>
+      <SearchContainer />
+    </header>
+  );
+};
