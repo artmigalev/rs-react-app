d38a13156e2c0e1fa2faac9ce34c7f50c1002dc4
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-08-03 13:54:07 +0300

    feat: should be in doc

---
+++ src/test-utils/Item.test.tsx
@@ -0,0 +1,13 @@
+import Item from "@/utils/item/Item";
+import { render, screen } from "@testing-library/react";
+
+describe("Item", () => {
+  const itemProps: string = "item-props";
+
+  it("should be in document", () => {
+    render(<Item str={itemProps} />);
+    screen.debug();
+    const item: HTMLElement = screen.getByRole("link", { name: /ITEM-PROPS/i });
+    expect(item).toBeInTheDocument();
+  });
+});
