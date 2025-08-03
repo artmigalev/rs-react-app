1e9599b354fe8862c43e445ad70cd7aa1216b5b8
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-08-03 11:32:54 +0300

    feat: install router

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "@app/App";
+import { BrowserRouter } from "react-router";
 
 const root = document.getElementById("root") as HTMLElement;
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
